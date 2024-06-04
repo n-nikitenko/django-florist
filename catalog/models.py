c5917c49e0347da8792758009851cb63c648ea4e
nnikitenko <nnikitenko@aorti.ru>
2024-06-04 13:15:38 +0300

    В приложении каталога созданы модели: - Product, - Category. У одной категории может быть много продуктов, но у одного продукта может быть только одна категория.
    Для поля с изображением  добавилены соответствующие настройки (MEDIA URL, MEDIA ROOT, настроен URL для отображения медиаданных) в проект, а также подключена библиотека для работы с изображениями
    Pillow

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.TextField(verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    created_at = models.DateTimeField(verbose_name='Дата/время создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата/время обновления', auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    title = models.TextField(verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    preview = models.ImageField(null=True, verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, verbose_name="Категория")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')
+
+    created_at = models.DateTimeField(verbose_name='Дата/время создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата/время обновления', auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
