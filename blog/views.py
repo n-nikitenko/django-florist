b64e35eb282905d1bd93c4a6d7b9ed45fc1d7344
nnikitenko <ninanineliya@gmail.com>
2024-06-19 23:08:59 +0300

    Вывод в список статей только тех, которые имеют положительный признак публикации.
    После успешного редактирования статьи происходит перенаправление пользователя на просмотр этой статьи.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
@@ -10,6 +9,10 @@ from blog.models import Article
 class ArticleListView(ListView):
     model = Article
     paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(is_published=True)
 
 
 class ArticleDetailView(DetailView):
@@ -25,7 +28,6 @@ class ArticleDetailView(DetailView):
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleForm
-    success_url = reverse_lazy('blog:articles')
 
     def form_valid(self, form):
         if form.is_valid():
@@ -34,11 +36,17 @@ class ArticleCreateView(CreateView):
             article.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('blog:article_detail', args=[self.object.pk])
+
 
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleForm
     success_url = reverse_lazy('blog:articles')
+
+    def get_success_url(self):
+        return reverse('blog:article_detail', args=[self.object.pk])
 
 
 class ArticleDeleteView(DeleteView):
