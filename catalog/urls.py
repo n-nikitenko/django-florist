07d7cba5d40934594d38ab394dd1edc6a18b247e
nnikitenko <ninanineliya@gmail.com>
2024-06-19 14:54:19 +0300

    использование namespace в ссылках

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,14 +6,14 @@ from catalog.views import home, contacts, services, prices, gallery, about, not_
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name=app_name),
-    path('contacts/', contacts, name=app_name),
-    path('services/', services, name=app_name),
-    path('prices/', prices, name=app_name),
-    path('prices/<int:pk>', product, name=app_name),
-    path('gallery/', gallery, name=app_name),
-    path('about/', about, name=app_name),
-    path('404/', not_found, name=app_name),
-    path('new_product/', new_product, name=app_name),
-    path('thanks/', thanks, name=app_name)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('services/', services, name='services'),
+    path('prices/', prices, name='prices'),
+    path('prices/<int:pk>', product, name='product_view'),
+    path('gallery/', gallery, name='gallery'),
+    path('about/', about, name='about'),
+    path('404/', not_found, name='404'),
+    path('new_product/', new_product, name='new_product'),
+    path('thanks/', thanks, name='thanks')
 ]
